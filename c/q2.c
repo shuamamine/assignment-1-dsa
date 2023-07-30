e5e74f003156ae4148aa2a6f756cdb1791d1c265
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-07-30 22:31:54 +0530

    Created q2.c

---
+++ c/q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main() 
+{
+int i, j,k;
+for (i=0; i<4; i++)
+{ 
+  for(j=i;j<3; j++)
+  {
+    printf(" ");
+  }
+  for (k=0; k < i+1; k++)
+  {
+   printf("%c", 65+i);
+  }
+ printf("\n");
+}
+}
