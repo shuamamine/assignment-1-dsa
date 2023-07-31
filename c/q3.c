630efb38e85f442ab4c60fd8ab5e07061da700c6
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-07-31 23:10:22 +0530

    Created q3.c

---
+++ c/q3.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+void main()
+{
+int count = 1; i,j,k;
+for (i=0; i<4, i++)
+{
+for(j=;j<3;j++) 
+{
+printf(" ");
+}
+for (k=0; k < i+1; k++) 
+{ 
+printf("%d", count);
+count++;
+}
+printf("\n");
+}
+}
