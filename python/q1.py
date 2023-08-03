43ca0c095f96176a1690e2add05f6e46f550f4ed
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-08-03 22:44:31 +0530

    Update q1.py

--- python/q1.py
+++ python/q1.py
@@ -1,1 +1,10 @@
-
+n = int(input("enter limit!!"))
+count = 1
+for i in range(0,n): 
+    for j in range(0,i+1): 
+         if count%2!=0: 
+           print("*", end="") 
+         else: 
+           print("#", end="")
+         count += 1
+    print()
